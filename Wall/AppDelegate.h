0b3d4147abf421e080b6a1597d018ba7ea38ad1a
Jiamao Zheng <jiamaoz@yahoo.com>
2015-04-06 03:34:07 -0500

    Initial Commit

---
+++ Wall/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Wall
+//
+//  Created by jiamao zheng on 4/6/15.
+//  Copyright (c) 2015 Jiamao Zheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ Wall/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Wall
+//
+//  Created by jiamao zheng on 4/6/15.
+//  Copyright (c) 2015 Jiamao Zheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
